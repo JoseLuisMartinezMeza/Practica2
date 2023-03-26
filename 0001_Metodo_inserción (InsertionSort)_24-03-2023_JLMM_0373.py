7e7317ec7d29cc20e83c9eeefe113dab4a991da8
JoseLuisMartinezMeza <127565873+JoseLuisMartinezMeza@users.noreply.github.com>
2023-03-25 20:36:49 -0600

    Update 0001_Metodo_inserción (InsertionSort)_24-03-2023_JLMM_0373.py

--- 0001_Metodo_inserción (InsertionSort)_24-03-2023_JLMM_0373.py
+++ 0001_Metodo_inserción (InsertionSort)_24-03-2023_JLMM_0373.py
@@ -6,12 +6,14 @@ Created on Fri Mar 24 21:34:15 2023
 """
 
 import random
-
+#Se importa el módulo random para generar números aleatorios.
 def ordenamiento_por_insercion(arreglo):
+     # Contamos cuántos elementos son más pequeños que el elemento actual
     for i in range(1, len(arreglo)):
         clave = arreglo[i]
         j = i-1
         while j >=0 and clave < arreglo[j] :
+            # Colocamos el elemento en su posición adecuada
                 arreglo[j+1] = arreglo[j]
                 j -= 1
         arreglo[j+1] = clave
